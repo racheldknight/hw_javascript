4a053e90afe6657d44af4e275a2437a6012630f6
Rachel Knight <dinkins.rachel@gmail.com>
2015-01-31 11:29:49 -0500

    Added numba function with strings

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 function howdy() {
-	console.log("Rachel Knight says Hi!");
+	console.log("Rachel Knight says what's up!");
 }
 
 howdy();
+
+var string = 7;
+
+var numba = function numba() {
+	if (string < 7) {
+		console.log("What a short little word!")
+	}
+
+	else if (string > 7) {
+		console.log("I'm sorry but that's too many to count.")
+	}
+
+	else if (string == 7) {
+		console.log("7, what a perfect choice!")
+	}
+
+}
+
+numba(7, "seventy", "university");
