774cff8e4e931a1356a43ae62122f20d3c8a9558
Rachel Knight <dinkins.rachel@gmail.com>
2015-01-31 09:55:57 -0500

    Added objects for 5 most recent Presidents

--- js/main.js
+++ js/main.js
@@ -4,4 +4,33 @@ var presidents = {
 	termLength: 2,
 	party: "Democrat",
 	yearsOfPresidency: 5,
+}
+
+	firstName: "George W", 
+	lastName: "Bush",
+	termLength: 2,
+	party: "Republican",
+	yearsOfPresidency: 8,
+}
+
+	firstName: "Bill", 
+	lastName: "Clinton",
+	termLength: 2,
+	party: "Democrat",
+	yearsOfPresidency: 8,
+}
+
+	firstName: "George H W", 
+	lastName: "Bush",
+	termLength: 1,
+	party: "Republican",
+	yearsOfPresidency: 4,
+}
+
+	firstName: "Ronald", 
+	lastName: "Reagan",
+	termLength: 2,
+	party: "Republican",
+	yearsOfPresidency: 8,
 };
+
