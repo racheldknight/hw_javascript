eb658207d56f8ba0b3afc9b01eb737088952d62a
Richard Kalehoff <richardkalehoff@gmail.com>
2015-02-03 09:00:15 -0500

    check string's length in 'numba'

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ howdy();
 
 
 var numba = function (string) {
-	if (string < 7) {
+	if (string.length < 7) {
 		console.log("What a short little word!");
 	}
 
-	else if (string > 7) {
+	else if (string.length > 7) {
 		console.log("I'm sorry but that's too many to count.");
 	}
 
-	else if (string == 7) {
+	else if (string.length == 7) {
 		console.log("7, what a perfect choice!");
 	}
 
